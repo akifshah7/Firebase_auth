4c1eec0a627fad620062ad579cad28abd04042d7
Akif Shah <akifshah7@gmail.com>
2024-01-20 14:28:49 +0530

    installed new dependencies

---
+++ src/components/Signup.component.tsx
@@ -0,0 +1,12 @@
+import '../index.css';
+import { Container } from '@mui/material';
+
+const SignUp = () => {
+  return (
+    <Container maxWidth="sm">
+
+    </Container>
+  )
+}
+
+export default SignUp
